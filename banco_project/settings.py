e4d326299cde41f2498b4ea9221a1868bc7a5252
rctorr <rictor@cuhrt.com>
2022-06-16 00:36:33 -0500

    Modificando settings para que se pueda acceder el proyecto de forma remota

--- banco_project/settings.py
+++ banco_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_+k)=_+xtu%z@8w1*_bil&!m1spvhr-w3vg)$xg7*+)38l^hl0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.1.66", "127.0.0.1"]
 
 
 # Application definition
