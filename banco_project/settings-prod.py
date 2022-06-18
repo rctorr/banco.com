5088a1d4aa481c934e78c120a4bc58c9219d4a5e
rctorr <rictor@cuhrt.com>
2022-06-18 19:59:28 +0000

    cambios necesario para poner en producción nuestra webapp.

--- banco_project/settings-prod.py
+++ banco_project/settings-prod.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +78,11 @@ WSGI_APPLICATION = 'banco_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rctorr$banco',
+        'USER': 'rctorr',
+        'PASSWORD': 'pythonsql',
+        'HOST': 'rctorr.mysql.pythonanywhere-services.com',
     }
 }
 
